2db06573a700a28c80e63d3244758b76a27e14ec
YusaOruc <h.yusa98@gmail.com>
2021-11-17 21:14:23 +0300

    1.0

---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogYorum by = new BlogYorum();
+        Context c = new Context();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            //var bloglar = c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger2 = c.Yorumlars.Take(3).ToList();
+            by.Deger3 = c.Blogs.Take(3).ToList();
+            return View(by);
+        }
+       
+        public ActionResult BlogDetay(int id)
+        {
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            by.Deger3 = c.Blogs.Take(3).ToList();
+            
+            //var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            return View(by);
+        }
+        public PartialViewResult YorumPartical()
+        {
+            by.Deger3 = c.Blogs.Take(3).ToList();
+            by.Deger2 = c.Yorumlars.Take(3).ToList();
+
+
+            return PartialView(by);
+        }
+
+       [HttpGet]
+       public PartialViewResult YorumYap(int id)
+       {
+            ViewBag.deger = id;
+            return PartialView();
+       }
+
+        [HttpPost]
+        public PartialViewResult YorumYap(Yorumlar y)
+        {
+            c.Yorumlars.Add(y);
+            c.SaveChanges();
+            return PartialView();
+        }
+    }
+}
---
+++ TravelTripProje/Controllers/YorumController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+using PagedList;
+using PagedList.Mvc;
+
+namespace TravelTripProje.Controllers
+{
+    public class YorumController : Controller
+    {
+        // GET: Yorum
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index(int sayfa=1)
+        {
+            //var deger = c.Yorumlars.ToList();
+            var deger = c.Yorumlars.ToList().ToPagedList(sayfa, 5);
+
+            return View(deger);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Yorumlar p)
+        {
+            c.Yorumlars.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var deger = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(deger);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult YorumGetir(int id)
+        {
+            var bl = c.Yorumlars.Find(id);
+            return View("YorumGetir", bl);
+        }
+        public ActionResult YorumGuncelle(Yorumlar b)
+        {
+            var blg = c.Yorumlars.Find(b.ID);
+            blg.KullaniciAdi = b.KullaniciAdi;
+            blg.Mail = b.Mail;
+            blg.Yorum = b.Yorum;
+            blg.Blogid = b.Blogid;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
